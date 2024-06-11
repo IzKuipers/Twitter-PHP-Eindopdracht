03565cdbb7a55893aed93ac05e5fd8838540b48d
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-06-11 11:12:17 +0200

    bruh

--- like.php
+++ like.php
@@ -52,5 +52,5 @@ try { // Probeer...
 } finally { // Ten slotte...
   sluitMysqli($connectie, $statement); // Probeer de connectie en tweede statement te sluiten
   $_SESSION["succes"] = 4;
-  "<script>history.back();</script>";
+  echo "<script>history.back();</script>";
 }
