e9ea21180d9961eab4af08697bb959ec95d66c46
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-06-12 15:37:59 +0200

    Update SQL file

--- importeer-mij.sql
+++ importeer-mij.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 11, 2024 at 06:52 PM
+-- Generation Time: Jun 12, 2024 at 03:36 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
